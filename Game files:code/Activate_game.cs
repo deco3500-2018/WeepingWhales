f89389ef6890f7822edd90f7aaa6606b598ae212
Jassmm <42561055+Jassmm@users.noreply.github.com>
2018-10-26 00:19:19 +1000

    Add files via upload

---
+++ Game files:code/Activate_game.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Activate_game: MonoBehaviour
+{
+
+
+    public void PlayGame ()
+    {
+        //SceneManager.GetActiveScene().buildIndex + 
+        SceneManager.LoadScene(1);
+        ScoreScriptTwo.scoreCount = 0;
+        ScoreScriptOne.scoreCount = 0;
+    }
+}
---
+++ Game files:code/TotalScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TotalScore : MonoBehaviour {
+
+    public static int scoreCount = 0;
+    Text score;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCount = ScoreScriptTwo.scoreCount + ScoreScriptOne.scoreCount;
+        string myString = scoreCount.ToString();
+        score.text = myString;
+    }
+}
